69b05e437c17b05f1da4d4f6d810ed09de36a630
Juan Camilo <camilo9020@gmail.com>
2016-07-18 13:29:08 -0500

    Reviews scaffold

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,15 +2,19 @@
 #
 # Table name: movies
 #
-#  id           :integer          not null, primary key
-#  title        :string
-#  description  :text
-#  movie_length :string
-#  director     :string
-#  rating       :string
-#  created_at   :datetime         not null
-#  updated_at   :datetime         not null
-#  user_id      :integer
+#  id                 :integer          not null, primary key
+#  title              :string
+#  description        :text
+#  movie_length       :string
+#  director           :string
+#  rating             :string
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  user_id            :integer
+#  image_file_name    :string
+#  image_content_type :string
+#  image_file_size    :integer
+#  image_updated_at   :datetime
 #
 
 class Movie < ActiveRecord::Base
--- app/models/user.rb
+++ app/models/user.rb
@@ -24,5 +24,6 @@ class User < ActiveRecord::Base
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-   has_many :movies      
+   has_many :movies    
+   has_many :reviews, dependent: :destroy
 end
